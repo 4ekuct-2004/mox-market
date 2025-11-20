e412efe864464e56159df063a195a3701b96214d
4EKUCT <aleshkaxxl123@gmail.com>
2025-11-20 14:06:57 +0700

    init

---
+++ src/main/java/ru/mox/mox_market/MoxMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.mox.mox_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoxMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoxMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mox/mox_market/MoxMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mox.mox_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoxMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
