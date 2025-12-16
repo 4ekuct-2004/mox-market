c7b6a8ed65def41989584923411764b7c64c6acd
4EKUCT <aleshkaxxl123@gmail.com>
2025-12-17 03:12:06 +0700

    сделал конфиг spring security

---
+++ src/main/java/ru/mox/mox_market/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.mox.mox_market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+
+                        // РЕСУРСЫ
+                        .requestMatchers(
+                                "/",
+                                "/register",
+                                "/login",
+                                "/market",
+                                "/market/**",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**"
+                        ).permitAll()
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        // API
+
+                        .requestMatchers("/api/register", "/api/public/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+        ;
+        return http.build();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
