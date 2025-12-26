db0b9b6432fdd69d6260616a813d6943a678d406
4EKUCT <aleshkaxxl123@gmail.com>
2025-12-27 01:29:26 +0700

    поправил фабричный метод у lotcarddto

--- src/main/java/ru/mox/mox_market/dto/dto_out/lot/LotCardDTO.java
+++ src/main/java/ru/mox/mox_market/dto/dto_out/lot/LotCardDTO.java
@@ -5,7 +5,11 @@ import lombok.extern.jackson.Jacksonized;
 import ru.mox.mox_market.dto.ItemDTO;
 import ru.mox.mox_market.dto.dto_out.BuyRequestOutDTO;
 import ru.mox.mox_market.dto.dto_out.SellRequestOutDTO;
+import ru.mox.mox_market.entity.requestEnt.BuyRequest;
+import ru.mox.mox_market.entity.requestEnt.SellRequest;
 import ru.mox.mox_market.entity.tradeEnt.Lot;
+
+import java.util.Comparator;
 
 @Builder
 @Jacksonized
@@ -21,6 +25,10 @@ public record LotCardDTO (
         return LotCardDTO.builder()
                 .id(lot.getId())
                 .item(ItemDTO.of(lot.getItem()))
+                .buyCount(lot.getBuyRequests().size())
+                .sellCount(lot.getSellRequests().size())
+                .bestBuy(BuyRequestOutDTO.of(lot.getBuyRequests().stream().max(Comparator.comparing(BuyRequest::getPrice)).orElse(null)))
+                .bestSell(SellRequestOutDTO.of(lot.getSellRequests().stream().min(Comparator.comparing(SellRequest::getPrice)).orElse(null)))
                 .build();
     }
 }
