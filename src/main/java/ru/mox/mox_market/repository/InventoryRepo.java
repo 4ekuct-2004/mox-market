a9d19321c241c9584ec453f57fe8455d35e53816
4EKUCT <aleshkaxxl123@gmail.com>
2025-12-28 00:14:29 +0700

    метод в InventoryRepo для получение общего объема продаж

--- src/main/java/ru/mox/mox_market/repository/InventoryRepo.java
+++ src/main/java/ru/mox/mox_market/repository/InventoryRepo.java
@@ -1,12 +1,21 @@
 package ru.mox.mox_market.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.mox.mox_market.entity.tradeEnt.Inventory;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface InventoryRepo extends JpaRepository<Inventory, Long> {
 
     Optional<Inventory> findByOwnerId(Long id);
+
+    @Query("""
+        select sum(t.price) from Transaction t
+            where t.buyerRequest.author.id = :userId or t.sellerRequest.author.id = :userId
+    """)
+    BigDecimal getInventoryMonetaryVolumeByOwnerId(@Param("userId") Long userId);
 
 }
